1877cd63d8d1fab6d4076f9b33847a254d52b04b
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-09-14 04:08:39 +0530

    makin resp verify page

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -5,6 +5,7 @@ import Poster from "../../../public/CINE.png"
 import Image from 'next/image'
 import axios from 'axios'
 import { useParams } from 'react-router-dom';
+import Gif from "../../../public/Festivities.gif"
 
 const Page = () => {
     const { id } = useParams();
@@ -12,13 +13,14 @@ const Page = () => {
         axios.post(`https://csi-examportal.onrender.com/api/verify/${id}`)
     })
   return (
-    <div className='w-screen h-screen object-contain mx-auto flex items-center justify-center'>
-        <div className='w-1/2'>
-            <Image src={Poster} width={450} height={450} alt='CINE'/>
+    <div className='w-full flex justify-center items-center md:flex-row flex-col-reverse md:p-6 p-2'>
+        <div className='md:w-1/2 w-full'>
+            <Image src={Poster} alt='CINE'/>
         </div>
-         <div className='flex flex-col items-center w-1/2'>
-        <p className='text-[50px] text-blue-700 font-semibold'>HOORAY!</p>
+         <div className='flex flex-col items-center text-center md:text-[35px] text-[20px] text-blue-700 font-bold md:w-1/2 md:p-6  w-full h-full'>
+        <p className=' '>HOORAY!</p>
         <p>You have successfully registered!</p>
+          <Image src={Gif} alt="Gif"/>
         </div>
         
        
