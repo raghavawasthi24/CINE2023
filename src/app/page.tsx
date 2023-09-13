89cd5f7369514594ee771f97f354da8bb69e155a
pushkar-28 <kharepushkar2804@gmail.com>
2023-09-14 03:03:44 +0530

    validated and env protected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,31 +88,14 @@ export default function Page() {
   const { toast } = useToast();
   const [csrfToken, setCsrfToken] = useState("");
   const [captcha, setRecaptcha] = useState("");
+  const [branchVerification, setBranchVerification] = useState(false);
 
-  // Function to extract branch name from studentNo
-  const getBranchFromStudentNo = (studentNo: string): string | undefined => {
-    // Extract the branch code (2 characters) from the studentNo
-    const branchCode = studentNo.slice(2, 4);
-
-    // Find the corresponding branch name in the branch_code array
-    const branchEntry = branch_code.find((entry) => entry.code === branchCode);
-
-    // Return the branch name if found, otherwise undefined
-    return branchEntry?.branch;
-  };
-
-  // Event handler for studentNo input field blur
-  const handleStudentNoBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    const studentNo = e.target.value;
-
-    // Extract the branch name from the student number
-    const branchName = getBranchFromStudentNo(studentNo);
-    const updatedBranchName = branchName?.substring(1, branchName.length - 1);
-    // Check if the branchName is not undefined
-    if (updatedBranchName) {
-      // Set the branch field value in the form
-      form.setValue("branch", updatedBranchName);
-    }
+  // Find the corresponding branch name in the branch_code array
+  const getBranchVerification = (data) => {
+    branch_code.map((item) => {
+      if (item.code === data.studentNo.substring(2, data.studentNo.length - 3))
+        setBranchVerification(true);
+    });
   };
 
   useEffect(() => {
@@ -144,7 +127,7 @@ export default function Page() {
           })
           .then((token) => {
             // You can send the 'token' to your server for verification.
-            console.log("reCAPTCHA token:", token);
+            // console.log("reCAPTCHA token:", token);
             setRecaptcha(token);
           });
       });
@@ -161,10 +144,19 @@ export default function Page() {
   function onSubmit(data: z.infer<typeof FormSchema>) {
     // Extract the student number from the form data
     const studentNo = data.studentNo;
-
+    // Check if the branch and student number match
+    getBranchVerification(data);
+    if (!branchVerification) {
+      toast({
+        variant: "destructive",
+        description: "Branch and Student Number does not match!",
+      });
+      return; // Do not proceed with form submission
+    }
     // Construct the expected email
     const expectedEmail = new RegExp(`^[a-z]+${studentNo}@akgec.ac.in$`);
     console.log(data, expectedEmail);
+
     // Check if the submitted email matches the expected email
     if (!expectedEmail.test(data.email)) {
       toast({
@@ -233,45 +225,45 @@ export default function Page() {
   return (
     <div className="flex flex-col p-6 md:flex-row items-center md:items-start">
       <div className="w-[90%] md:w-1/2">
-        <Image src={Poster} alt="CINE"/>
+        <Image src={Poster} alt="CINE" />
       </div>
       <div className="w-full md:w-1/2 md:shadow-2xl md:ml-6 md:p-6 p-3 sm:w-[80%]">
-        <p className="font-semibold text-center md:text-[30px] text-[20px] text-[#0A012A] my-4">Hey! Get Yourself Registered</p>
+        <p className="font-semibold text-center md:text-[30px] text-[20px] text-[#0A012A] my-4">
+          Hey! Get Yourself Registered
+        </p>
         <Form {...form}>
-        
           <form
             onSubmit={form.handleSubmit(onSubmit)}
             className="flex flex-col gap-4"
           >
-      
-          
-              {formfields.map((fields, index) => {
-                if (fields.type === "input")
-                  return (
-                    <InputField
-                      form={form}
-                      name={fields.key}
-                      label={fields.label}
-                      placeholder={fields.placeHolder}
-                      key={index}
-                    />
-                  );
-                else
-                  return (
-                    <SelectField
-                      form={form}
-                      name={fields.key}
-                      label={fields.label}
-                      placeholder={fields.placeHolder}
-                      options={fields.options || []}
-                      key={index}
-                    />
-                  );
-              })}
-  
+            {formfields.map((fields, index) => {
+              if (fields.type === "input")
+                return (
+                  <InputField
+                    form={form}
+                    name={fields.key}
+                    label={fields.label}
+                    placeholder={fields.placeHolder}
+                    key={index}
+                  />
+                );
+              else
+                return (
+                  <SelectField
+                    form={form}
+                    name={fields.key}
+                    label={fields.label}
+                    placeholder={fields.placeHolder}
+                    options={fields.options || []}
+                    key={index}
+                  />
+                );
+            })}
+
             <div className="w-full flex justify-center my-6">
               <Button type="submit" className="w-[200px] bg-[#0A012A]">
-              <BsFillSendFill className="w-4 h-4 mr-2 text-white"/>Submit
+                <BsFillSendFill className="w-4 h-4 mr-2 text-white" />
+                Submit
               </Button>
             </div>
           </form>
