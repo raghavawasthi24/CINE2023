029ae206c959eb0f375132e37310d50380298348
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-09-14 07:38:57 +0530

    some

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -11,7 +11,13 @@ import Gif from "../../../public/Festivities.gif"
 const Page = () => {
     const { id } = useParams();
     useEffect(()=>{
-        axios.post(`https://csi-examportal.onrender.com/api/verify/${id}`)
+        axios.post(`https://csi-examportal.onrender.com/api/verify/${id}`).then((res)=>{
+          console.log(res)
+        })
+          .catch((err)=>{
+            console.log(err)
+          
+        })
     })
   return (
     <div className='w-full flex justify-center items-center md:flex-row flex-col-reverse md:p-6 p-2'>
@@ -20,7 +26,7 @@ const Page = () => {
         </div>
          <div className='flex flex-col items-center text-center md:text-[3rem] text-[25px] font-spartan md:w-1/2 md:p-7 w-full h-full' style={{ fontFamily: 'League Spartan' }}>
         <p className='mb-5'>HOORAY!</p>
-        <p>You&apos;ve Registered Successfully!</p>
+        <p>You've Registered Successfully!</p>
           <Image src={Gif} alt="Gif"/>
         </div>
         
