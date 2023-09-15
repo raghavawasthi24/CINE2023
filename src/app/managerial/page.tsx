a8d551bf4f4bbcfb36b8ffcdfac236ed5e090b80
pushkar-28 <kharepushkar2804@gmail.com>
2023-09-15 23:25:57 +0530

    managerial page added

---
+++ src/app/managerial/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+import Poster from "../../../public/CINE.png";
+import Image from "next/image";
+
+import Gif from "../../../public/Festivities.gif";
+
+const Page = () => {
+  return (
+    <div className="w-full flex justify-center items-center md:flex-row flex-col-reverse md:p-6 p-2">
+      <div className="md:w-1/2 w-full">
+        <Image src={Poster} alt="CINE" />
+      </div>
+      <div
+        className="flex flex-col items-center text-center md:text-[3rem] text-[25px] font-spartan md:w-1/2 md:p-7 w-full h-full"
+        style={{ fontFamily: "League Spartan" }}
+      >
+        <p className="mb-5">HOORAY!</p>
+        <p>You&apos;ve Registered Successfully for Managerial Round!</p>
+        <Image src={Gif} alt="Gif" />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
